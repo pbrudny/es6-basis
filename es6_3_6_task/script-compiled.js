0b07b5aae3eac71a8a58f746e341a5342d60b2df
pbrudny <pbrudny@gmail.com>
2017-09-28 01:39:10 +0200

    Add styling

---
+++ es6_3_6_task/script-compiled.js
@@ -0,0 +1,40 @@
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+class Stopwatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+    }
+
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+    }
+
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+}
+
+const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
+
+var startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+var stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop);
