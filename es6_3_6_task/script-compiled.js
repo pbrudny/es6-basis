f2b928f18d6f56dc5f429c94e56ebcf002445ef3
pbrudny <pbrudny@gmail.com>
2017-09-28 11:02:58 +0200

    Add reset button

--- es6_3_6_task/script-compiled.js
+++ es6_3_6_task/script-compiled.js
@@ -11,7 +11,6 @@ class Stopwatch {
         this.running = false;
         this.display = display;
         this.reset();
-        this.print(this.times);
     }
 
     reset() {
@@ -20,10 +19,41 @@ class Stopwatch {
             seconds: 0,
             miliseconds: 0
         };
+        this.print(this.times);
     }
 
     print() {
         this.display.innerText = this.format(this.times);
+    }
+
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+
+    calculate() {
+        this.times.miliseconds += 10;
+        if (this.times.miliseconds >= 1000) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
     }
 
     format(times) {
@@ -37,4 +67,7 @@ var startButton = document.getElementById('start');
 startButton.addEventListener('click', () => stopwatch.start());
 
 var stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => stopwatch.stop);
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+var stopButton = document.getElementById('reset');
+stopButton.addEventListener('click', () => stopwatch.reset());
